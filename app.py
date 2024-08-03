392265ce172da70a02bc645f1d228b657aaa833f
Yanai <yanaiparan@gmail.com>
2024-08-03 12:19:00 +0000

    resolved conflicts

--- app.py
+++ app.py
@@ -7,13 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-<<<<<<< HEAD
-    return 'Hello from Flask!'
-
-=======
     return "<p>Hello World!</p>"
 
 @app.route("/health")
 def health():
     return "<p>everythings Great!</p>"
->>>>>>> dcb6dd9a3557f0cdc6621b16bc1f2cf302896e10
